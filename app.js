918bebb3c7fd18b771906c161f371991d2f6262e
Vlad <vladut_stoian@live.co.uk>
2022-06-24 14:16:14 +0100

    Made it so that it counts your score. Tells you when you win and resets

--- app.js
+++ app.js
@@ -7,74 +7,59 @@ const diceImage = document.getElementById("diceImage")
 const imageSource = ["./Dice/dice1.jpeg", "./Dice/dice2.jpeg", "./Dice/dice3.jpeg", "./Dice/dice4.jpeg", "./Dice/dice5.jpeg", "./Dice/dice6.jpeg"]
 
 
-function rollDice()
+function rollDice(diceRoll)
 {
-    let diceRoll = 0
-    let failRoll = false
-    let targetRoll = 20
-    let diceTotal = 0
-    console.log(diceRoll)
 
 
-    while (failRoll == false && diceTotal < targetRoll)
-    {
-        diceRoll = Math.ceil(Math.random() * 6)
-        console.log(diceRoll)
 
+
+    diceRoll = Math.ceil(Math.random() * 6)
         if (diceRoll == 1)
         {
             document.getElementById("diceImage").src = imageSource[0]
             // alert("You have rolled a 1, you lost")
-            diceTotal += diceRoll
         }
         if (diceRoll == 2)
         {
             document.getElementById("diceImage").src = imageSource[1]
-            alert(`You rolled a ${diceRoll}, press Ok to continue`)
-            diceTotal += diceRoll
         }
         if (diceRoll == 3)
         {
             document.getElementById("diceImage").src = imageSource[2]
-            alert(`You rolled a ${diceRoll}, press Ok to continue`)
-            diceTotal += diceRoll
         }
         if (diceRoll == 4)
         {
             document.getElementById("diceImage").src = imageSource[3]
-            alert(`You rolled a ${diceRoll}, press Ok to continue`)
-            diceTotal += diceRoll
         }
         if (diceRoll == 5)
         {
             document.getElementById("diceImage").src = imageSource[4]
-            alert(`You rolled a ${diceRoll}, press Ok to continue`)
-            diceTotal += diceRoll
         }
         if (diceRoll == 6)
         {
             document.getElementById("diceImage").src = imageSource[5]
-            alert(`You rolled a ${diceRoll}, press Ok to continue`)
-            diceTotal += diceRoll
         }
-
-        if (diceTotal >= targetRoll)
-        {
-            failRoll = true
-            alert("You have won the game")
-            return diceRoll
-        }
+        return diceRoll
+  
     }
-
-
-}
 
 
 
 useKey.addEventListener("click", () => {
-    
-    diceNumber.textContent = rollDice()
+    let diceRoll = 0
+    let diceTotal = 0
+    // let failRoll = false
+    // let targetRoll = 20
 
+    diceNumber.textContent = rollDice(diceRoll)
+    console.log(diceTotal)
+
+
+
+    // if (diceTotal >= targetRoll)
+    // {
+    //     alert("You have won the game")
+    // }
 })
 
 
