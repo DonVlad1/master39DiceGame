7efdaa00b4739496efb8747d2c707f718b76a6a9
Vlad <vladut_stoian@live.co.uk>
2022-06-24 16:23:30 +0100

    Removed alerts, made text show win/loss, added colours

--- app.js
+++ app.js
@@ -4,82 +4,56 @@ const removeButton = document.getElementById("remove")
 const keyPress = document.getElementById("keyPress")
 const diceNumber = document.getElementById("diceNumber")
 const diceImage = document.getElementById("diceImage")
+const gameStatus = document.getElementById("gameStatus")
 const imageSource = ["./Dice/dice1.jpeg", "./Dice/dice2.jpeg", "./Dice/dice3.jpeg", "./Dice/dice4.jpeg", "./Dice/dice5.jpeg", "./Dice/dice6.jpeg"]
 
+function replaceImage(diceRoll)
+{
+    for (let index = 1; index <= 6; index++) {
+        if (diceRoll == index)
+            {
+                document.getElementById("diceImage").src = imageSource[index - 1]
+            }
+    }
+}
 
 function rollDice(diceRoll)
 {
-
-    let failRoll = false
-    let targetRoll = 20
-
-
         diceRoll = Math.ceil(Math.random() * 6)
+        replaceImage(diceRoll)
 
-        if (diceRoll == 1)
-        {
-            document.getElementById("diceImage").src = imageSource[0]
-
-        }
-        if (diceRoll == 2)
-        {
-            document.getElementById("diceImage").src = imageSource[1]
-        }
-        if (diceRoll == 3)
-        {
-            document.getElementById("diceImage").src = imageSource[2]
-        }
-        if (diceRoll == 4)
-        {
-            document.getElementById("diceImage").src = imageSource[3]
-        }
-        if (diceRoll == 5)
-        {
-            document.getElementById("diceImage").src = imageSource[4]
-        }
-        if (diceRoll == 6)
-        {
-            document.getElementById("diceImage").src = imageSource[5]
-        }
-
-        // if (diceTotal >= targetRoll)
-        // {
-        //     failRoll = true
-        //     alert("You have won the game")
-        //     return diceRoll
-        // }
-        console.log(diceRoll)
         return diceRoll
-
 }
 
 
 let diceTotal = 0
 let diceRoll = 0
 let actualRoll = 0
-let diceTarget = prompt("Please Enter how many points to win:")
+let diceTarget = 15 //prompt("Please Enter how many points to win:")
 
 useKey.addEventListener("click", () => {
 
     actualRoll = rollDice(diceRoll)
+    gameStatus.style.color = "black"
 
 
     if (diceTotal <= diceTarget)
     {
         diceTotal += actualRoll
-        diceNumber.textContent = diceTotal
+        gameStatus.textContent = diceTotal
+
     }
     if (diceTotal > diceTarget)
     {
-        alert(`You have ${diceTotal} points, you win`)
+        gameStatus.textContent = `You have won the game with ${diceTotal} points. press Roll the Dice to start a new game`
+        gameStatus.style.color = "darkgreen"
         diceTotal = 0
-        diceNumber.textContent = diceTotal
+
     }
     else if (actualRoll == 1)
     {
-        alert("You have rolled a 1, you lost")
         diceTotal = 0
-        diceNumber.textContent = 0
-
+        gameStatus.textContent = "You have rolled a 1, you lost the game, press Roll the Dice to start a new game"
+        gameStatus.style.color = "darkred"
     }
 })
