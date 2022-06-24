ced4036da136ed0cf6c10f2fcdd6d4134f379eff
Vlad <vladut_stoian@live.co.uk>
2022-06-24 13:21:18 +0100

    Added while loop but don't like it

--- app.js
+++ app.js
@@ -4,27 +4,77 @@ const removeButton = document.getElementById("remove")
 const keyPress = document.getElementById("keyPress")
 const diceNumber = document.getElementById("diceNumber")
 const diceImage = document.getElementById("diceImage")
-const imageSource = "./Dice/dice2.jpeg"
+const imageSource = ["./Dice/dice1.jpeg", "./Dice/dice2.jpeg", "./Dice/dice3.jpeg", "./Dice/dice4.jpeg", "./Dice/dice5.jpeg", "./Dice/dice6.jpeg"]
 
 
 function rollDice()
 {
-    diceRoll = Math.round(Math.random())
+    let diceRoll = 0
+    let failRoll = false
+    let targetRoll = 20
+    let diceTotal = 0
     console.log(diceRoll)
 
-    // if (diceRoll == 1)
-    // {
-    //     document.getElementById("diceImage").src = "./Dice/dice1.jpeg"
-    // }
-    return [diceRoll, diceImage]
+
+    while (failRoll == false && diceTotal < targetRoll)
+    {
+        diceRoll = Math.ceil(Math.random() * 6)
+        console.log(diceRoll)
+
+        if (diceRoll == 1)
+        {
+            document.getElementById("diceImage").src = imageSource[0]
+            // alert("You have rolled a 1, you lost")
+            diceTotal += diceRoll
+        }
+        if (diceRoll == 2)
+        {
+            document.getElementById("diceImage").src = imageSource[1]
+            alert(`You rolled a ${diceRoll}, press Ok to continue`)
+            diceTotal += diceRoll
+        }
+        if (diceRoll == 3)
+        {
+            document.getElementById("diceImage").src = imageSource[2]
+            alert(`You rolled a ${diceRoll}, press Ok to continue`)
+            diceTotal += diceRoll
+        }
+        if (diceRoll == 4)
+        {
+            document.getElementById("diceImage").src = imageSource[3]
+            alert(`You rolled a ${diceRoll}, press Ok to continue`)
+            diceTotal += diceRoll
+        }
+        if (diceRoll == 5)
+        {
+            document.getElementById("diceImage").src = imageSource[4]
+            alert(`You rolled a ${diceRoll}, press Ok to continue`)
+            diceTotal += diceRoll
+        }
+        if (diceRoll == 6)
+        {
+            document.getElementById("diceImage").src = imageSource[5]
+            alert(`You rolled a ${diceRoll}, press Ok to continue`)
+            diceTotal += diceRoll
+        }
+
+        if (diceTotal >= targetRoll)
+        {
+            failRoll = true
+            alert("You have won the game")
+            return diceRoll
+        }
+    }
+
+
 }
 
 
 
 useKey.addEventListener("click", () => {
-    let diceRoll = 0
-    diceNumber.textContent = rollDice(0)
-    document.getElementById("diceImage").src = imageSource
+    
+    diceNumber.textContent = rollDice()
+
 })
 
 
